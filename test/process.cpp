f7eed9376e0b1c19addc2b88dfe2e353126ec712
StoneHappy <18017158729@163.com>
2022-09-10 23:40:01 +0800

    🧪 test: process

---
+++ test/process.cpp
@@ -0,0 +1,69 @@
+#include <cstdint>
+#include <functional>
+#include <gtest/gtest.h>
+#include <entt/process/process.hpp>
+#include <entt/process/scheduler.hpp>
+#include <iostream>
+
+struct Process0 : entt::process<Process0, int>
+{
+	Process0(std::function<void()> upd, std::function<void()> abort)
+		: on_update(upd), on_aborted(abort) {}
+
+	void update(delta_type, void*)
+	{
+		on_update();
+	}
+	 void aborted() {
+        on_aborted();
+    }
+	std::function<void()> on_update;
+	std::function<void()> on_aborted;
+};
+
+struct Process1 : entt::process<Process1, int>
+{
+	Process1(std::function<void()> upd)
+		: on_update(upd) {}
+
+	void update(delta_type, void*)
+	{
+		on_update();
+	}
+	std::function<void()> on_update;
+	std::function<void()> on_aborted;
+};
+
+TEST(ECSbegin, process0)
+{
+	entt::scheduler<int> scheduler{};
+
+	bool updated = false;
+	bool aborted = false;
+
+	scheduler.attach<Process0>( [&updated]() { updated = true; },
+        [&aborted]() { aborted = true; });
+	
+	ASSERT_NE(scheduler.size(), 0u);
+    ASSERT_FALSE(scheduler.empty());
+
+	scheduler.update(0);
+	scheduler.abort(true);
+	ASSERT_TRUE(updated);
+	ASSERT_TRUE(aborted);
+}
+
+TEST(ECSbegin, process1)
+{
+	entt::scheduler<int> scheduler{};
+
+	scheduler.attach<Process1>( []() { std::cout << 1 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 2 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 3 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 4 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 5 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 6 << std::endl; });
+	scheduler.attach<Process1>( []() { std::cout << 7 << std::endl; });
+
+	scheduler.update(0);
+}
